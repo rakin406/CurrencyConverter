7b660dbc6fe0d191a9f755014ddef22cd33f9ac5
rakin406 <rakinhacker@gmail.com>
2021-05-18 17:35:43 +0600

    update

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun updateResult(v: View?) {
+    fun updateResult() {
         val et = findViewById<EditText>(R.id.cash)
         val inputText = et.text.toString()
-        if (!inputText.matches("")) {
+        if (inputText.isNotBlank()) {
             // Convert currency to two decimal place
             val dollar = inputText.toDouble()
-            val taka = Math.round(dollar * 84.81 * 100).toDouble() / 100
+            val taka = (dollar * 84.81 * 100).roundToInt().toDouble() / 100
 
             // Show result
             val result = findViewById<TextView>(R.id.result)
