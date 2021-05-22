2465d1bac4854fa2a52cbfe226012cc856b021da
rakin406 <rakinhacker@gmail.com>
2021-05-22 10:57:12 +0600

    todo

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val currencies = arrayOf("Dollar", "Taka")
+        val currencies = arrayOf("BDT", "Canadian Dollar", "Chinese Yuan", "Euro", "Indian Rupee",
+            "Jamaican Dollar", "Japanese Yen", "USD")
+
         val fromCurrency: Spinner = findViewById(R.id.fromCurrency)
         val toCurrency: Spinner = findViewById(R.id.toCurrency)
 
@@ -34,16 +36,9 @@ class MainActivity : AppCompatActivity() {
         convertButton.setOnClickListener {
             val originalCurrency = fromCurrency.selectedItem.toString()
             val convertedCurrency = toCurrency.selectedItem.toString()
-
-            // Set exchange rate based on both currencies
-            val exchangeRate = if (originalCurrency == "Taka" && convertedCurrency == "Dollar") {
-                0.012
-            } else {
-                84.82
-            }
-
             val et = findViewById<EditText>(R.id.input)
             val inputText = et.text.toString()
+
             if (inputText.isNotBlank()) {
                 // Convert currency to two decimal place
                 val amount = inputText.toDouble()
@@ -52,7 +47,7 @@ class MainActivity : AppCompatActivity() {
                 val convertedAmount: Double = if (originalCurrency == convertedCurrency) {
                     amount
                 } else {
-                    ((amount * exchangeRate) * 100.0).roundToInt() / 100.0
+                    ((amount * getExchangeRate(originalCurrency, convertedCurrency)) * 100.0).roundToInt() / 100.0
                 }
 
                 // Show result
@@ -60,6 +55,62 @@ class MainActivity : AppCompatActivity() {
                 val formatted = "$convertedAmount $convertedCurrency"
                 result.text = formatted
             }
+        }
+    }
+
+    /*
+     * Get exchange rate by checking the currencies
+     */
+    private fun getExchangeRate(originalCurrency: String, convertedCurrency: String): Double {
+        // TODO: Manage all currencies and exchange rates
+        return if (originalCurrency == "BDT") {
+            when (convertedCurrency) {
+                "Canadian Dollar" -> 0.014
+                "Chinese Yuan" -> 0.076
+                "Euro" -> 0.0097
+                "Indian Rupee" -> 0.86
+                "Jamaican Dollar" -> 1.76
+                "Japanese Yen" -> 1.28
+                "USD" -> 0.012
+                else -> {
+                    1.0
+                }
+            }
+        } else if (originalCurrency == "Canadian Dollar") {
+            when (convertedCurrency) {
+                "BDT" -> 70.30
+                "Chinese Yuan" -> 5.33
+                "Euro" -> 0.68
+                "Indian Rupee" -> 60.42
+                "Jamaican Dollar" -> 124.01
+                "Japanese Yen" -> 90.26
+                "USD" -> 0.83
+                else -> {
+                    1.0
+                }
+            }
+        } else if (originalCurrency == "USD") {
+            when (convertedCurrency) {
+                "BDT" -> 84.83
+                "Canadian Dollar" -> 1.21
+                "Chinese Yuan" -> 6.43
+                "Euro" -> 0.82
+                "Indian Rupee" -> 72.91
+                "Jamaican Dollar" -> 149.65
+                "Japanese Yen" -> 108.93
+                else -> {
+                    1.0
+                }
+            }
+        } else if (originalCurrency == "BDT") {
+            when (convertedCurrency) {
+                "USD" -> 0.012
+                else -> {
+                    1.0
+                }
+            }
+        } else {
+            1.0
         }
     }
 }
